46e50109158ea985e91f03edc4aa6ad2b4cc14af
deependujha <deependujha21@gmail.com>
2026-03-29 21:57:41 +0530

    update

--- src/vegam/constants.py
+++ src/vegam/constants.py
@@ -2,6 +2,6 @@
 
 CPP_GITIGNORE_FILE_URL = "https://raw.githubusercontent.com/github/gitignore/refs/heads/main/C%2B%2B.gitignore"
 PYTHON_GITIGNORE_FILE_URL = "https://raw.githubusercontent.com/github/gitignore/refs/heads/main/Python.gitignore"
-GO_PYTHON_GITIGNORE_FILE_URL = (
+GO_GITIGNORE_FILE_URL = (
     "https://raw.githubusercontent.com/github/gitignore/refs/heads/main/Go.gitignore"
 )
